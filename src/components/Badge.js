1cd5da7deee47e648944f71f120c58d3fa5aaac9
JuliaGrisales <jgrisales@gmail.com>
2020-07-20 18:19:39 +0200

    Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -10,14 +10,14 @@ class Badge extends React.Component {
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <img className="Badge__avatar" src={this.props.avatarUrl} alt="Avatar" />
           <h1>
-            Julian <br /> Grisales
+            {this.props.firstName} <br /> {this.props.lastName}
           </h1>
         </div>
         <div className="Badge__section-info">
-          <h3>Full Stack Developer</h3>
-          <div>@jgrisalescode</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>{this.props.twitter}</div>
         </div>
         <div className="Badge__footer">#platziconf</div>
       </div>
--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ import Badge from "./components/Badge"
 const container = document.getElementById("app")
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(<Badge firstName="Richar" lastName="Kaufman" jobTitle="Frontend Senior Dev" twitter="@jgrisalescode" avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"></Badge>, container)
