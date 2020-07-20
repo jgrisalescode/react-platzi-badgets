9a5166ede4c89b7eedd2ea6603f07d1b5937dc11
JuliaGrisales <jgrisales@gmail.com>
2020-07-20 18:06:32 +0200

    Como aplicar estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,25 @@
 import React from "react"
 import confLogo from "../images/badge-header.svg"
+import "./styles/Badge.css"
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
           <h1>
             Julian <br /> Grisales
           </h1>
         </div>
-        <div>
-          <p>Full Stack Developer</p>
-          <p>@jgrisalescode</p>
+        <div className="Badge__section-info">
+          <h3>Full Stack Developer</h3>
+          <div>@jgrisalescode</div>
         </div>
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     )
   }
