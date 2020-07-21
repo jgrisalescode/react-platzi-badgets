e9403725b73ce4e2963ed7531fdf2aea21177400
JuliaGrisales <jgrisales@gmail.com>
2020-07-21 13:53:28 +0200

    Enlazando eventos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import "./styles/BadgeNew.css"
 import header from "../images/badge-header.svg"
 import NavBar from "../components/NavBar"
 import Badge from "../components/Badge"
+import BadgeForm from "../components/BadgeForm"
 
 class BadgeNew extends React.Component {
   render() {
@@ -14,8 +15,11 @@ class BadgeNew extends React.Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge firstName="Richard" lastName="Kaufman" twitter="@sparragus" jogTitle="Frontend Enginier" avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon" />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
