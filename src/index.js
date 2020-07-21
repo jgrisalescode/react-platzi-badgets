33192044b0ff2dd5727dcf9f146faee27006500e
JuliaGrisales <jgrisales@gmail.com>
2020-07-21 13:35:09 +0200

    Nuestra primera pagina

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "bootstrap/dist/css/bootstrap.css"
 import "./global.css"
-import Badge from "./components/Badge"
+import BadgeNew from "./pages/BadgeNew"
 
 const container = document.getElementById("app")
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge firstName="Richar" lastName="Kaufman" jobTitle="Frontend Senior Dev" twitter="@jgrisalescode" avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"></Badge>, container)
+ReactDOM.render(<BadgeNew />, container)
